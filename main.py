0034ec2ab6f97757025661b7b6deb7a9144e334c
nhsiang <nhsiang4912@hotmail.com>
2026-07-28 08:14:46 -0500

    edge detection update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import cv2
 import cv2 as cv
+import numpy as np
 
 img = cv.imread('./img/track_first_person_pov.jpg')
 
 # edge detection
-canny = cv2.Canny(img, 150, 150)
+blurred = cv.GaussianBlur(img, (7,7), 1)
+canny = cv2.Canny(blurred, 150, 150)
 
 cv.imshow('canny', canny)
 cv.waitKey(0)
